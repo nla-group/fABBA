e729e3654632eebfba850f609d3888664c5b4869
the null <42580530+chenxinye@users.noreply.github.com>
2023-01-18 14:03:41 +0000

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,7 +1,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+autodoc_mock_imports = ['matplotlib']
 
 # -- Project information -----------------------------------------------------
 import sys
