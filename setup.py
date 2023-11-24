852a2d7785a606c56c03fb6d91545f580c86dd7b
the null <42580530+chenxinye@users.noreply.github.com>
2023-11-24 12:30:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -46,12 +46,7 @@ setup_args = {'name':"fABBA",
                     "Topic :: Scientific/Engineering",
                     "Operating System :: Microsoft :: Windows",
                     "Operating System :: Unix",
-                    "Programming Language :: Python :: 3",
-                    "Programming Language :: Python :: 3.6",
-                    "Programming Language :: Python :: 3.7",
-                    "Programming Language :: Python :: 3.8",
-                    "Programming Language :: Python :: 3.9",
-                    "Programming Language :: Python :: 3.10"
+                    "Programming Language :: Python :: 3"
                     ],
         'description':"An efficient aggregation method for the symbolic representation of temporal data",
         'long_description_content_type':'text/x-rst',
