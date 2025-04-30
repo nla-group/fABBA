6cf6bb2bde59d91aa2b87593c352353ac3a2a7e8
the-null <cclcquant@yahoo.com>
2025-04-30 04:01:07 +0200

    fix modules load

--- fABBA/__init__.py
+++ fABBA/__init__.py
@@ -10,14 +10,13 @@ except (ModuleNotFoundError, ValueError):
     from .separate.aggregation import aggregate
     
     
-__version__ = '1.3.0'
+__version__ = '1.3.1'
 from .load_datasets import load_images, loadData
 from .fabba import (image_compress, image_decompress, ABBAbase, ABBA,
                                       get_patches, patched_reconstruction, fABBA,
                                       symbolsAssign, fillna)
 
 
-from .qabba import *
 
 from .fabba import _compress as compress
 from .fabba import _inverse_compress as inverse_compress
@@ -27,3 +26,6 @@ from .jabba.jabba import fastABBA, JABBA
 from .jabba import jabba
 from .jabba import preprocessing
 from .jabba.fkmns import sampledKMeansInter as fkmeans
+
+from .jabba import qabba
+from .jabba.qabba import *
